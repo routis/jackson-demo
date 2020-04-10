8a2a177258f5528fb17d45c55944d9fe0dc3e319
babis <haralampos.routis@intrasoft-intl.com>
2020-04-10 17:19:36 +0300

    Added DisplayName in test methods

--- src/test/java/routis/examples/jacksondemo/JacksonDemoApplicationTests.java
+++ src/test/java/routis/examples/jacksondemo/JacksonDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package routis.examples.jacksondemo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -11,7 +12,6 @@ import org.springframework.core.io.ClassPathResource;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @JsonTest
-
 class JacksonDemoApplicationTests {
 
     private static final Student MARK_TWAIN = new Student("Mark", "Twain",null);
@@ -20,14 +20,14 @@ class JacksonDemoApplicationTests {
     private JacksonTester<Student> json;
 
     @Test
+    @DisplayName("A null Map<String,String> is mapped as null in json")
     public void testSerialize() throws Exception {
-
         JsonContent<Student> content = this.json.write(MARK_TWAIN);
-        System.out.println(content.toString());
         assertThat(content).isEqualToJson(new ClassPathResource("mark.twain.expected.json"));
     }
 
     @Test
+    @DisplayName("An empty json array, when using accept-empty-array-as-null-object=true, is mapped to a Map<String,String> as null")
     public void testDeserialize() throws Exception {
         String content = "{\n" +
                 "  \"firstName\": \"Mark\",\n" +
