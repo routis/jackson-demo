7ddb20490d7950f0107ce0f544187ad858d1fa96
babis <haralampos.routis@intrasoft-intl.com>
2020-04-10 14:54:21 +0300

    clean uo

--- src/test/java/routis/examples/jacksondemo/JacksonDemoApplicationTests.java
+++ src/test/java/routis/examples/jacksondemo/JacksonDemoApplicationTests.java
@@ -1,6 +1,5 @@
 package routis.examples.jacksondemo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 import org.springframework.boot.test.json.ObjectContent;
 import org.springframework.core.io.ClassPathResource;
-
-import java.util.HashMap;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
